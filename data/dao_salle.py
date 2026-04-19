8919aa31cba335ff2632945cd1e054cd8cb897e8
Nourredinne <noureddine.boussekine@monboreal.ca>
2026-04-19 15:50:55 -0400

    Ajout de la methode insert_salle

--- data/dao_salle.py
+++ data/dao_salle.py
@@ -1,6 +1,6 @@
 import json
 import mysql.connector
-
+from models.salle import Salle
 
 
 class DataSalle:
@@ -16,3 +16,19 @@ class DataSalle:
         )
 
         return connection
+
+    def insert_salle(self, salle):
+        connection = self.get_connection()
+        cursor = connection.cursor()
+
+        requete = """
+        INSERT INTO salle (code, description, categorie, capacite)
+        VALUES (%s, %s, %s, %s)
+        """
+        valeurs = (salle.code, salle.description, salle.categorie, salle.capacite)
+
+        cursor.execute(requete, valeurs)
+        connection.commit()
+
+        cursor.close()
+        connection.close()
