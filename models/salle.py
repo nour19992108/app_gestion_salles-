c6610b0b50dadf18c8d976f5af67439d94463f57
Nourredinne <noureddine.boussekine@monboreal.ca>
2026-04-19 15:10:24 -0400

    Creation de la classe Salle et definir ses attributs

---
+++ models/salle.py
@@ -0,0 +1,7 @@
+class Salle:
+
+    def _init_(self, code, description, categorie, capacite):
+        self.code = code
+        self.description = description
+        self.categorie = categorie
+        self.capacite = capacite
