c67d69057d154b6b42298e5d99bb8095f29cb7f1
Nourredinne <noureddine.boussekine@monboreal.ca>
2026-04-19 22:39:30 -0400

    ajout methode modfier_salle

--- services/service_salle.py
+++ services/service_salle.py
@@ -13,3 +13,12 @@ def ajouter_salle(self, salle):
 
     self.dao.insert_salle(salle)
     return True, "✅ Salle ajoutée"
+def modifier_salle(self, salle):
+    if not salle.code or not salle.description or not salle.categorie or not salle.capacite:
+        return False, "❌ Données manquantes"
+
+    if salle.capacite < 1:
+        return False, "❌ Capacité invalide"
+
+    self.dao.update_salle(salle)
+    return True, "✅ Salle modifiée"
